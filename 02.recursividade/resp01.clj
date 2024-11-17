49f83551ec12d44c69219e86eab770618a32af0e
Matheus Galdino <122388600+MatheusGaldinoo@users.noreply.github.com>
2024-11-16 21:37:28 -0300

    Curso de Clojure

--- 02.recursividade/resp01.clj
+++ 02.recursividade/resp01.clj
@@ -5,4 +5,4 @@
 
 (msg-rec (read-line))
 
-; O que acontece é que a pilha estoura, ultrapassa o limite da pilha de execução do java por se tratar de uma infinitude de comandos
+; O que acontece é que a pilha estoura, ultrapassa o limite da pilha de execução do java, pois se trata de uma infinitude de comandos.
